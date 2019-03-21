2b73432c950bb74107e76458a906c8cee7069d1d
Ogün Özdemir <ogun.ozdemir33@gmail.com>
2019-03-22 02:44:07 +0300

    first commit

---
+++ yazlab/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace yazlab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblsayi = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnbitir = new System.Windows.Forms.Button();
+            this.btnbasla = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "THREADS";
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.AutoSize = true;
+            this.lblsonuc.Location = new System.Drawing.Point(481, 124);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(0, 13);
+            this.lblsonuc.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(423, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Sonuç:";
+            // 
+            // lblsayi
+            // 
+            this.lblsayi.AutoSize = true;
+            this.lblsayi.Location = new System.Drawing.Point(481, 99);
+            this.lblsayi.Name = "lblsayi";
+            this.lblsayi.Size = new System.Drawing.Size(0, 13);
+            this.lblsayi.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Sıradaki Sayı:";
+            // 
+            // btnbitir
+            // 
+            this.btnbitir.BackColor = System.Drawing.Color.Red;
+            this.btnbitir.Location = new System.Drawing.Point(453, 27);
+            this.btnbitir.Name = "btnbitir";
+            this.btnbitir.Size = new System.Drawing.Size(84, 33);
+            this.btnbitir.TabIndex = 8;
+            this.btnbitir.Text = "Bitir";
+            this.btnbitir.UseVisualStyleBackColor = false;
+            this.btnbitir.Click += new System.EventHandler(this.btnbitir_Click);
+            // 
+            // btnbasla
+            // 
+            this.btnbasla.BackColor = System.Drawing.Color.Red;
+            this.btnbasla.Location = new System.Drawing.Point(359, 27);
+            this.btnbasla.Name = "btnbasla";
+            this.btnbasla.Size = new System.Drawing.Size(88, 33);
+            this.btnbasla.TabIndex = 7;
+            this.btnbasla.Text = "Başla";
+            this.btnbasla.UseVisualStyleBackColor = false;
+            this.btnbasla.Click += new System.EventHandler(this.btnbasla_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(356, 124);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Araştırılan Sayı:";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(359, 219);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(165, 68);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(37, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(212, 95);
+            this.listBox1.TabIndex = 21;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(691, 88);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(129, 199);
+            this.listBox2.TabIndex = 22;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(688, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "ID   -   Asal Sayı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 381);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblsayi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnbitir);
+            this.Controls.Add(this.btnbasla);
+            this.Name = "Form1";
+            this.Text = "YAZ-LAB II";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblsayi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnbitir;
+        private System.Windows.Forms.Button btnbasla;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox label9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ yazlab/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yazlab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string veritabani = "Data Source=CONFI-PC;Initial Catalog=yazlab;Integrated Security=True";
+        SqlConnection baglanti = new SqlConnection(veritabani);
+
+        bool calis = false;
+        int tambolen;
+        int threaddegeri;
+
+
+        private void btnbasla_Click(object sender, EventArgs e)
+        {
+            if (calis == true)
+            {
+                MessageBox.Show("Program zaten çalışıyor.");
+            }
+
+            else
+            {
+                calis = true;
+                Thread basla = new Thread(delegate () { motor(); });
+
+                basla.Start();
+            }
+
+            
+        }
+
+        public void motor()
+        {
+            while (calis == true)
+            {
+                System.Windows.Forms.Form.CheckForIllegalCrossThreadCalls = false;
+
+                Thread start3 = new Thread(delegate () { });
+               
+                listBox1.Items.Clear();
+                baglanti.Open();
+                int b = Convert.ToInt32(lblsayi.Text);
+                double c = Math.Sqrt(b);
+                int tvn = Convert.ToInt32(c);
+                int threadsayisi = 0;
+                int tvnid = 0;
+             
+                SqlCommand komut2 = new SqlCommand("SELECT * from asal where sayilar <= '" + tvn + "' ", baglanti);
+                SqlDataReader oku2 = komut2.ExecuteReader();
+                while (oku2.Read())
+                {
+                  
+                    tvnid = Convert.ToInt32(oku2["ID"]);
+
+                }
+
+
+                oku2.Close();
+                baglanti.Close();
+
+                int d = 0;
+                baglanti.Open();
+                SqlCommand komut21 = new SqlCommand("SELECT count(*) from asal where sayilar<="+tvn, baglanti);
+                d = (Int32)komut21.ExecuteScalar();
+                baglanti.Close();
+
+                if (d <= 100)
+                {
+                    threadsayisi = 2;
+                }
+                if (d > 100)
+                {
+                    threadsayisi = Convert.ToInt32((d / 100) + 1);
+                }
+
+                label4.Text = "Kullanılacak Thread Sayısı: " + threadsayisi.ToString();
+
+                Thread[] start2 = new Thread[threadsayisi];
+
+                bool[] asaldizi = new bool[threadsayisi];
+                int pay = tvnid / threadsayisi;
+                int kalan = tvnid % threadsayisi;
+
+                int parametre1 = 1, parametre2 = pay;
+                int deneme = 0;
+
+                while (deneme < threadsayisi)
+                {
+
+                    deneme++;
+
+                    start2[deneme - 1] = new Thread(() =>
+                    {
+
+                        asaldizi[deneme - 1] = asilthread(parametre1, parametre2, b);
+                    }
+                         );
+
+                    listBox1.Items.Add("Thread "+deneme+" -  taban ID:" + parametre1 + "   tavan ID:" + parametre2);
+
+                    start2[deneme - 1].Start();
+                    start2[deneme - 1].Join(1000);
+                    
+                    if (deneme == threadsayisi - 1)
+                    {
+                        parametre1 = pay + parametre1;
+                        parametre2 = pay + parametre2 + kalan;
+                    }
+                    else
+                    {
+                        parametre1 = pay + parametre1;
+                        parametre2 = pay + parametre2;
+                    }
+
+                }
+
+
+                bool bitti = true;
+
+                while (bitti)
+                {
+
+                    for (int i = 0; i < threadsayisi; i++)
+                    {
+
+
+                        if (start2[i].Join(1))
+                        {
+
+                            bitti = false;
+                        }
+                        else
+                        {
+                            bitti = true;
+                            break;
+                        }
+                    }
+
+                    if (bitti == false)
+                    {
+
+                        bool kontrol = true;
+                        for (int i = 0; i < threadsayisi; i++)
+                        {
+                            if (asaldizi[i] == false)
+                            {
+                                kontrol = false;
+                                threaddegeri = i+1;
+                                break;
+                            }
+
+                        }
+
+                        if (kontrol == true)
+                        {
+                            label9.Text = "Sayı asal sayıdır!";
+
+                            start3 = new Thread(delegate ()
+                            {
+
+                                threadkayit(b);
+                            }
+                                   );
+                            start3.Start();
+
+                            while (true)
+                            {
+                                if (start3.Join(1))
+                                {
+                                    b = Convert.ToInt32(lblsayi.Text);
+                                    lblsonuc.Text = b.ToString();
+                                    b += 2;
+                                    lblsayi.Text = b.ToString();
+                                    break;
+                                }
+
+
+                            }
+                        }
+                        else
+                        {
+                            label9.Text = "Sayı asal değildir!\nThread "+threaddegeri+" da bulunan "+ tambolen+" sayısına tam olarak bölünmektedir.";
+
+                            b = Convert.ToInt32(lblsayi.Text);
+                            lblsonuc.Text = b.ToString();
+                            b += 2;
+                            lblsayi.Text = b.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool asilthread(int paraa1, int paraa2, int sayi)
+        {
+            bool durum = true;
+            for (int i = paraa1; i <= paraa2; i++)
+            {
+         
+                SqlConnection baglanti = new SqlConnection(veritabani);
+                baglanti.Open();
+
+                SqlCommand komut = new SqlCommand("SELECT sayilar from asal where ID = '" + i + "' ", baglanti);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    string a = oku["sayilar"].ToString();
+                    int d = Convert.ToInt32(a);
+
+                    if (sayi % d == 0)
+                    {
+                        durum = false;
+                        tambolen = d;
+                        break;
+                    }
+
+                }
+
+                oku.Close();
+                baglanti.Close();
+                }
+
+                return durum;
+        }
+
+        private void threadkayit(int p11)
+        {
+            SqlConnection baglanti = new SqlConnection(veritabani);
+
+            int id = 0;
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("SELECT TOP 1 * from asal order by ID DESC ", baglanti);
+            SqlDataReader oku2 = komut2.ExecuteReader();
+            if (oku2.Read())
+            {
+
+                id = Convert.ToInt32(oku2["ID"]) + 1;
+            }
+
+            oku2.Close();
+            baglanti.Close();
+
+            baglanti.Open();
+            string kayit = "insert into asal(ID,sayilar) values ('"+id+"',@sayilar)";
+            SqlCommand komut3 = new SqlCommand(kayit, baglanti);
+            komut3.Parameters.AddWithValue("@sayilar", p11.ToString());
+            komut3.ExecuteNonQuery();
+            baglanti.Close();
+
+
+           
+        }
+
+
+        void sondeger()
+        {
+
+
+            baglanti.Open();
+            SqlCommand komut5 = new SqlCommand("SELECT max(sayilar) from asal ", baglanti);
+            SqlDataReader dr = komut5.ExecuteReader();
+
+            while (dr.Read())
+            {
+
+                String a = dr[""].ToString();
+                int sayi = Convert.ToInt32(a);
+                sayi = sayi + 2;
+                lblsayi.Text = sayi.ToString();
+            }
+            dr.Close();
+            baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            sondeger();
+
+        }
+
+        private void btnbitir_Click(object sender, EventArgs e)
+        {
+
+            if (calis == false)
+            {
+                MessageBox.Show("Program zaten çalışmıyor.");
+            }
+
+            else
+            {
+                calis = false;
+
+                listBox2.Items.Clear();
+                baglanti.Open();
+                SqlCommand komut2 = new SqlCommand("SELECT * from asal", baglanti);
+                SqlDataReader oku2 = komut2.ExecuteReader();
+                while (oku2.Read())
+                {
+
+                    listBox2.Items.Add(Convert.ToInt32(oku2["ID"]) + "   -   " + Convert.ToInt32(oku2["sayilar"]));
+
+                }
+
+                oku2.Close();
+                baglanti.Close();
+            }   
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
